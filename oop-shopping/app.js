b1e7ae00a6ee55bbb966939191d79e2b377da425
Amir <154999211+MRMirsoleymani@users.noreply.github.com>
2025-05-12 02:13:37 +0330

    Add files via upload

---
+++ oop-shopping/app.js
@@ -0,0 +1,24 @@
+import Cart from "./models/Cart.js";
+import Products from "./models/Products.js";
+import { fetchData } from "./utils/httpReq.js";
+
+const productsNode = document.getElementById("products");
+const cartListNode = document.getElementById("cart-list");
+const totalPriceNode = document
+  .getElementById("total-price")
+  .querySelector("span");
+
+const render = async () => {
+  const productsData = await fetchData();
+
+  const cartInstance = new Cart(cartListNode, totalPriceNode);
+  const ProductsInstance = new Products(
+    productsNode,
+    productsData,
+    cartInstance
+  );
+
+  ProductsInstance.showProducts();
+};
+
+document.addEventListener("DOMContentLoaded", render);
